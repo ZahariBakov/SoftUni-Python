3c3b354faa9694aec9e87901d15b77673bf40842
ZahariBakov <zaharibakov@gmaail.com>
2022-05-26 14:16:55 +0300

    Add folder "Conditional Statements - Lab" and exercise.

---
+++ Python-Programming-Basics/Conditional Statements - Lab/greater_number.py
@@ -0,0 +1,7 @@
+first_number = int(input())
+second_number = int(input())
+
+if first_number >= second_number:
+    print(first_number)
+elif first_number < second_number:
+    print(second_number)
---
+++ Python-Programming-Basics/Conditional Statements - Lab/password_guess.py
@@ -0,0 +1,6 @@
+passcode = input()
+
+if passcode == "s3cr3t!P@ssw0rd":
+    print('Welcome')
+else:
+    print('Wrong password!')
