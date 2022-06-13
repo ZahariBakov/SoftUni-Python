2a9b2c51bf63f688bfc5935e591aaa24545e3f05
zaharibakov <zaharibakov@gmail.com>
2022-06-13 09:35:58 +0300

    Update "smallest_of_three_numbers".

--- Programming Fundamentals with Python/Functions - Exercise/smallest_of_three_numbers.py
+++ Programming Fundamentals with Python/Functions - Exercise/smallest_of_three_numbers.py
@@ -4,16 +4,7 @@ third_number = int(input())
 
 
 def smallest(a, b, c):
-    if a < b:
-        if a < c:
-            return a
-        else:
-            return c
-    else:
-        if b < c:
-            return b
-        else:
-            return c
+    return min(a, b, c)
 
 
 print(smallest(first_number, second_number, third_number))
