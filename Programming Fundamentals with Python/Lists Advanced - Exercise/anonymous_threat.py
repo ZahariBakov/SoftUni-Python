cf87ce3e2e219e9a3e496081dfacc8c92eeb5ea1
zaharibakov <zaharibakov@gmail.com>
2022-06-21 09:57:10 +0300

    Update "anonymous_threat".

--- Programming Fundamentals with Python/Lists Advanced - Exercise/anonymous_threat.py
+++ Programming Fundamentals with Python/Lists Advanced - Exercise/anonymous_threat.py
@@ -5,35 +5,35 @@ while command != "3:1":
     if "merge" in command:
         start_index = int(command.split()[1])
         end_index = int(command.split()[2])
-        temporary_list = []
+        temporary_list_out = []
         for i in range(start_index, end_index + 1):
             if len(strings) > start_index:
-                temporary_list.append(strings.pop(start_index))
-        temporary_string = "".join(temporary_list)
-        strings.insert(start_index, temporary_string)
+                temporary_list_out.append(strings.pop(start_index))
+        temporary_list_get = "".join(temporary_list_out)
+        strings.insert(start_index, temporary_list_get)
     elif "divide" in command:
         index = int(command.split()[1])
         divider = int(command.split()[2])
-        temporary_string = list(strings.pop(index))
-        length = len(temporary_string) // divider
-        temporary_list = []
+        temporary_list_get = list(strings.pop(index))
+        length = len(temporary_list_get) // divider
+        temporary_list_out = []
         adding_characters = ""
-        length_temporary = len(temporary_string)
+        length_temporary = len(temporary_list_get)
         for i in range(length_temporary):
-            adding_characters += temporary_string.pop(0)
+            adding_characters += temporary_list_get.pop(0)
             if len(adding_characters) == length:
-                temporary_list.append(adding_characters)
+                temporary_list_out.append(adding_characters)
                 adding_characters = ""
-                if len(temporary_list) == divider:
+                if len(temporary_list_out) == divider:
                     break
-        if temporary_string != []:
-            adding_characters = temporary_list.pop()
-            for i in temporary_string:
-                adding_characters += temporary_string.pop(0)
-            temporary_list.append(adding_characters)
-        if temporary_list != []:
+        if temporary_list_get != []:
+            adding_characters = temporary_list_out.pop()
+            for i in temporary_list_get:
+                adding_characters += temporary_list_get.pop(0)
+            temporary_list_out.append(adding_characters)
+        if temporary_list_out != []:
             for i in range(divider):
-                strings.insert(index, temporary_list.pop(0))
+                strings.insert(index, temporary_list_out.pop(0))
                 index += 1
 
     command = input()
