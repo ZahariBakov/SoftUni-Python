eb476dffe4393a4b9e24aee660c4cfeeacd503c5
zaharibakov <zaharibakov@gmail.com>
2022-06-23 12:46:58 +0300

    Update "anonymous_threat".

--- Programming Fundamentals with Python/Lists Advanced - Exercise/anonymous_threat.py
+++ Programming Fundamentals with Python/Lists Advanced - Exercise/anonymous_threat.py
@@ -5,6 +5,10 @@ while command != "3:1":
     if "merge" in command:
         start_index = int(command.split()[1])
         end_index = int(command.split()[2])
+        if start_index < 0:
+            start_index = 0
+        if end_index >= len(strings):
+            end_index = len(strings) - 1
         temporary_list_out = []
         for i in range(start_index, end_index + 1):
             if len(strings) > start_index:
@@ -15,17 +19,16 @@ while command != "3:1":
         index = int(command.split()[1])
         divider = int(command.split()[2])
         temporary_list_get = list(strings.pop(index))
-        length = len(temporary_list_get) // divider
+        numbers_of_characters = len(temporary_list_get) // divider
         temporary_list_out = []
         adding_characters = ""
         length_temporary = len(temporary_list_get)
         for i in range(length_temporary):
-            adding_characters += temporary_list_get.pop(0)
-            if len(adding_characters) == length:
-                temporary_list_out.append(adding_characters)
-                adding_characters = ""
-                if len(temporary_list_out) == divider:
-                    break
+            if len(temporary_list_out) < divider:
+                adding_characters += temporary_list_get.pop(0)
+                if len(adding_characters) == numbers_of_characters:
+                    temporary_list_out.append(adding_characters)
+                    adding_characters = ""
         if temporary_list_get != []:
             adding_characters = temporary_list_out.pop()
             for i in temporary_list_get:
