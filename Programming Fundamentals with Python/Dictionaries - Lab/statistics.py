b0002b8a4e183590f68f8935fc0ac5aab55279c0
zaharibakov <zaharibakov@gmail.com>
2022-07-18 18:28:24 +0300

    Add "statistics".

---
+++ Programming Fundamentals with Python/Dictionaries - Lab/statistics.py
@@ -0,0 +1,20 @@
+products = {}
+
+while True:
+    command = input()
+
+    if command == "statistics":
+        break
+
+    command = command.split(": ")
+    if command[0] not in products:
+        products[command[0]] = int(command[1])
+
+    else:
+        products[command[0]] += int(command[1])
+
+print("Products in stock:")
+for (key, value) in products.items():
+    print(f"- {key}: {value}")
+print(f"Total Products: {len(products.keys())}")
+print(f"Total Quantity: {sum(products.values())}")
