5b530f811882c5bf37abb9869c7d5ffb012e7a7a
zaharibakov <zaharibakov@gmail.com>
2022-07-19 18:10:19 +0300

    Update "03_capitals".

--- Programming Fundamentals with Python/Dictionaries - Exercise/03_capitals.py
+++ Programming Fundamentals with Python/Dictionaries - Exercise/03_capitals.py
@@ -3,5 +3,5 @@ cities = input().split(", ")
 
 dictionary = dict(zip(countries, cities))
 
-data = [f"{key} -> {value}" for key, value in dictionary.items()]
-print("\n".join(data))
+for key, value in dictionary.items():
+    print(f"{key} -> {value}")
