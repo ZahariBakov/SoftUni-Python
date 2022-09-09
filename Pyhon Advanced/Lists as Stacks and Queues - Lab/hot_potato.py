91844e457a611c32eb48690d09a76989a02f1693
ZahariBakov <zaharibakov@gmail.com>
2022-09-09 10:39:04 +0300

    Add "hot_potato".

---
+++ Pyhon Advanced/Lists as Stacks and Queues - Lab/hot_potato.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+players_string = input()
+tosses_count = int(input())
+
+player = deque(players_string.split(" "))
+current_toss = 0
+
+while len(player) > 1:
+    current_toss += 1
+    current_player = player.popleft()
+
+    if current_toss == tosses_count:
+        current_toss = 0
+        print(f"Removed {current_player}")
+
+    else:
+        player.append(current_player)
+
+winner = player.popleft()
+print(f"Last is {winner}")
