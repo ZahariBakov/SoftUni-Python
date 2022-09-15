9cdfa9c3debdca438a936f42b61246ffe649b1a5
ZahariBakov <zaharibakov@gmail.com>
2022-09-15 20:27:12 +0300

    Update "softuni-party".

--- Pyhon Advanced/Tuples and Sets - Lab/softuni-party.py
+++ Pyhon Advanced/Tuples and Sets - Lab/softuni-party.py
@@ -1,15 +1,15 @@
 n = int(input())
-guestс_list = set()
+guests_list = set()
 
 for _ in range(n):
     guest = input()
-    guestс_list.add(guest)
+    guests_list.add(guest)
 
 guest_coming = input()
 
 while guest_coming != "END":
-    guestс_list.discard(guest_coming)
+    guests_list.discard(guest_coming)
     guest_coming = input()
 
-print(len(guestс_list))
-print("\n".join(sorted(guestс_list)))
+print(len(guests_list))
+print("\n".join(sorted(guests_list)))
