288914c62c79b2be32b5bface1d7d71ca7dc6f0e
ZahariBakov <zaharibakov@gmail.com>
2022-09-21 10:11:46 +0300

    Update "crossroads".

--- Pyhon Advanced/Lists as Stacks and Queues - Exercise/crossroads.py
+++ Pyhon Advanced/Lists as Stacks and Queues - Exercise/crossroads.py
@@ -24,19 +24,22 @@ while cars_and_command:
     if command == "green":
         time = green_light
 
-    elif time <= 0:
+    elif time == 0:
         continue
 
     else:
         if time + free_window < len(command):
+            crash_char = command[time + free_window]
             print("A crash happened!")
-            print(f"{command} was hit at e.")
+            print(f"{command} was hit at {crash_char}.")
             crash = True
             break
 
         else:
             passed_cars += 1
             time -= len(command)
+            if time < 0 :
+                time = 0
 
 if not crash:
     print("Everyone is safe.")
