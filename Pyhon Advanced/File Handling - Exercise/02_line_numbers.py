fbfa50147da9e35afa2835a6507b3e334e1800f7
ZahariBakov <zaharibakov@gmail.com>
2022-10-04 17:15:30 +0300

    Update file "02_line_numbers". Add text file

--- Pyhon Advanced/File Handling - Exercise/02_line_numbers.py
+++ Pyhon Advanced/File Handling - Exercise/02_line_numbers.py
@@ -16,9 +16,9 @@ def letters_symbols_count(text):
 
 file_path = './text.txt'
 
-with open(file_path) as file:
+with open(file_path) as input_file, open('./output.txt', 'w') as output_file:
     idx = 1
-    for line in file:
+    for line in input_file:
         count_letter, count_symbols = letters_symbols_count(line)
-        print(f"Line {idx}: {line.strip()} ({count_letter})({count_symbols})")
+        output_file.write(f"Line {idx}: {line.strip()} ({count_letter})({count_symbols})\n")
         idx += 1
