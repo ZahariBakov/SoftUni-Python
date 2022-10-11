5102cb4ddd48cb8c69dfeaea3926023e2cde7f91
ZahariBakov <zaharibakov@gmail.com>
2022-10-11 16:56:26 +0300

    Update file "01_calculate_logarithm".

--- Pyhon Advanced/Modules - Lab/01_calculate_logarithm.py
+++ Pyhon Advanced/Modules - Lab/01_calculate_logarithm.py
@@ -3,7 +3,11 @@ from math import log
 numbers = int(input())
 base = input()
 
-if base == "natural":
-    print(f"{log(numbers):.2f}")
-else:
-    print(f"{log(numbers, int(base)):.2f}")
+# if base == "natural":
+#     print(f"{log(numbers):.2f}")
+# else:
+#     print(f"{log(numbers, int(base)):.2f}")
+
+result = log(numbers) if base == "natural" else log(numbers, int(base))
+
+print(f"{result:.2f}")
