e62a85a8f4bcf75129e708b3f21cf0809651053f
ZahariBakov <zaharibakov@gmail.com>
2022-10-15 09:35:19 +0300

    Update file "03_shopping_cart".

--- Pyhon Advanced/Exam Preparation/03_shopping_cart.py
+++ Pyhon Advanced/Exam Preparation/03_shopping_cart.py
@@ -1,4 +1,5 @@
 def shopping_cart(*args):
+    result = ''
     meals = {
         "Pizza": [],
         "Dessert": [],
@@ -6,56 +7,44 @@ def shopping_cart(*args):
     }
 
     for el in args:
-        meal = el[0]
-        product = el[1]
-        if el == "Stop":
+        if "Stop" in el:
             break
-        if meal == "Pizza" and len(meals[meal]) == 4:
-            continue
-        elif meal == "Soup" and len(meals[meal]) == 3:
-            continue
-        elif meal == "Desert" and len(meals[meal]) == 2:
-            continue
-        if product not in meals[meal]:
-            meals[meal].append(product)
+        meal, product = el
+        for meal_type, number_product in (("Pizza", 4), ("Soup", 3), ("Dessert", 2)):
+            if meal == meal_type and len(meals[meal]) != number_product and product not in meals[meal]:
+                meals[meal].append(product)
 
-    for value in meals.values():
-        if len(value) > 0:
-            break
-    else:
-        return "No products in the cart!"
+    for p_meal, p_product in sorted(meals.items(), key=lambda x: (-len(x[1]), x[0])):
+        result += f"{p_meal}:\n"
+        for item in sorted(p_product):
+            result += f" - {item}\n"
 
-    sorted_meals = sorted(meals.items(), key=lambda x: (-len(x[1]), x[0]))
-    result = ''
-    for el in sorted_meals:
-        result += f"{el[0]}:\n"
-        sorted_products = sorted(el[1])
-        for product in sorted_products:
-            result += f" - {product}\n"
+    if len(result) == 22:
+        result = "No products in the cart!"
 
     return result
 
 
-# print(shopping_cart(
-#     ('Pizza', 'ham'),
-#     ('Soup', 'carrots'),
-#     ('Pizza', 'cheese'),
-#     ('Pizza', 'flour'),
-#     ('Dessert', 'milk'),
-#     ('Pizza', 'mushrooms'),
-#     ('Pizza', 'tomatoes'),
-#     'Stop',
-# ))
+print(shopping_cart(
+    ('Pizza', 'ham'),
+    ('Soup', 'carrots'),
+    ('Pizza', 'cheese'),
+    ('Pizza', 'flour'),
+    ('Dessert', 'milk'),
+    ('Pizza', 'mushrooms'),
+    ('Pizza', 'tomatoes'),
+    'Stop',
+))
 
-# print(shopping_cart(
-#     ('Pizza', 'ham'),
-#     ('Dessert', 'milk'),
-#     ('Pizza', 'ham'),
-#     'Stop',
-# ))
+print(shopping_cart(
+    ('Pizza', 'ham'),
+    ('Dessert', 'milk'),
+    ('Pizza', 'ham'),
+    'Stop',
+))
 
-# print(shopping_cart(
-#     'Stop',
-#     ('Pizza', 'ham'),
-#     ('Pizza', 'mushrooms'),
-# ))
+print(shopping_cart(
+    'Stop',
+    ('Pizza', 'ham'),
+    ('Pizza', 'mushrooms'),
+))
