6aa493d3beb53828753d494653a37c6b15bc90ca
ZahariBakov <zaharibakov@gmail.com>
2022-11-20 17:42:14 +0200

    Update exercise "04_squares".

--- Python OOP/Iterators and Generators - Lab/04_squares.py
+++ Python OOP/Iterators and Generators - Lab/04_squares.py
@@ -1,9 +1,26 @@
-def squares(n):
-    num = 1
+# def squares(n):
+#     num = 1
+#
+#     while num <= n:
+#         yield num ** 2
+#         num += 1
 
-    while num <= n:
-        yield num ** 2
-        num += 1
+
+class squares:
+    def __init__(self, end):
+        self.start = 1
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start > self.end:
+            raise StopIteration
+
+        self.start += 1
+
+        return self.start ** 2
 
 
 print(list(squares(5)))
