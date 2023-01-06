893ff30304d2a3459be04f9be3a0c1884d31e40e
ZahariBakov <zaharibakov@gmail.com>
2023-01-06 17:50:47 +0200

    Add another views in file "views".

--- python_web_basics/django_introduction/django_introduction/tasks/urls.py
+++ python_web_basics/django_introduction/django_introduction/tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from django_introduction.tasks.views import show_bare_minimum_view
+from django_introduction.tasks.views import show_bare_minimum_view, show_all_tasks
 
 urlpatterns = (
+    # http://localhost:8000/tasks/
     path('', show_bare_minimum_view),
+    # http://localhost:8000/tasks/all/
+    path('all/', show_all_tasks),
 )
--- python_web_basics/django_introduction/django_introduction/tasks/views.py
+++ python_web_basics/django_introduction/django_introduction/tasks/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django import http
+from django_introduction.tasks.models import Task
 
 
 # Create your views here.
 def show_bare_minimum_view(request):
     return http.HttpResponse('It works')
+
+
+def show_all_tasks(request):
+    all_tasks = Task.objects\
+        .order_by('priority')\
+        .all()
+    # [name(id), name(id)]
+    result = ', '.join(f'{t.name}({t.id})' for t in all_tasks)
+
+    return http.HttpResponse(result)
