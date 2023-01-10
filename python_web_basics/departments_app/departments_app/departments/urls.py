d356a29a5f742ab100d13d9bdb0264553e6b15db
ZahariBakov <zaharibakov@gmail.com>
2023-01-10 18:16:13 +0200

    Update views. Add first templates.

--- python_web_basics/departments_app/departments_app/departments/urls.py
+++ python_web_basics/departments_app/departments_app/departments/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from departments_app.departments.views import show_departments
+from departments_app.departments.views import show_departments, show_departments_details
 
 urlpatterns = [
     # /departments/
     path('', show_departments),
 
     # /departments/{id}/
-    path('<departemnt_id>/', show_departments),
+    path('<departemnt_id>/', show_departments_details),
 
     # /departments/int/{id}/
-    path('int/<int:department_id>/', show_departments),
+    path('int/<int:department_id>/', show_departments_details),
 ]
 
 # paths = (
--- python_web_basics/departments_app/departments_app/departments/views.py
+++ python_web_basics/departments_app/departments_app/departments/views.py
@@ -1,10 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 
 # Create your views here.
-def show_departments(request, *args, **kwargs):
-    body = f'path=: {request.path}, args={args}, kwargs={kwargs}'
+# Without render
+# def show_departments(request: HttpRequest, *args, **kwargs):
+#     print(request.method)
+#     print(request.GET)  # query params (?param1=value1 & param2=value2)
+#     print(request.POST)  # body of HTTP request
+#     print(request.get_port())
+#     print(request.get_host())
+#     print(request.headers)
+#
+#     order_by = request.GET.get('order_by', 'name')
+#
+#     body = f'path: {request.path}, args={args}, kwargs={kwargs}, order_by: {order_by}'
+#
+#     return HttpResponse(body)
+
+
+def show_departments(request: HttpRequest, *args, **kwargs):
+    context = {
+        'page_title': 'Departments',
+        'method': request.method,
+        'order_by': request.GET.get('order_by', 'name'),
+    }
+
+    return render(request, 'departments/all.html', context)
+
+
+def show_departments_details(request: HttpRequest, department_id):
+    body = f'path: {request.path}, id: {department_id}'
 
     return HttpResponse(body)
-
