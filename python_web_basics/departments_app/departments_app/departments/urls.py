93309aaad16b40a1c6799953b703a1bfc28d0c69
ZahariBakov <zaharibakov@gmail.com>
2023-01-10 17:05:14 +0200

    Update "urls" file.

--- python_web_basics/departments_app/departments_app/departments/urls.py
+++ python_web_basics/departments_app/departments_app/departments/urls.py
@@ -2,7 +2,26 @@ from django.urls import path
 from departments_app.departments.views import sample_view
 
 urlpatterns = [
+    # /departments/
     path('', sample_view),
+
+    # /departments/{id}/
     path('<departemnt_id>/', sample_view),
+
+    # /departments/int/{id}/
     path('int/<int:department_id>/', sample_view),
 ]
+
+# paths = (
+#     '',
+#     '<department_id>/',
+#     'int/<int:department_id>/',
+# )
+#
+# urlpatterns = [path(url, sample_view) for url in paths]
+
+
+# urlpatterns = ()
+# urlpatterns += (path('', sample_view),)
+# urlpatterns += (path('<department_id>/', sample_view),)
+# urlpatterns += (path('int/<int:department_id>/', sample_view),)
