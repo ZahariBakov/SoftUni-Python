cd4116104a9edb7b8c0478b2e4f689b175a09150
ZahariBakov <zaharibakov@gmail.com>
2023-05-18 10:50:30 +0300

    Create project "expenses_tracker". Add all HTML files. Create "base.html". Create all views. Add static files. Create "web" app.

---
+++ Python Web Basics/Exam Preparation/expenses_tracker/expenses_tracker/web/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+
+def show_home(request):
+    return render(request, 'home-with-profile.html')
+
+
+def create_expense(request):
+    return render(request, 'expense-create.html')
+
+
+def edit_expense(request, pk):
+    return render(request, 'expense-edit.html')
+
+
+def delete_expense(request, pk):
+    return render(request, 'expense-delete.html')
+
+
+def show_profile(request):
+    return render(request, 'profile.html')
+
+
+def create_profile(request):
+    return render(request, 'home-no-profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'profile-edit.html')
+
+
+def delete_profile(request):
+    return render(request, 'profile-delete.html')
