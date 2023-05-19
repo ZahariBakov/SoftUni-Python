92645cf44258521cb61340e6b9feb38bc91654c3
ZahariBakov <zaharibakov@gmail.com>
2023-05-19 17:15:30 +0300

    Update project "expenses_tracker".

--- Python Web Basics/Exam Preparation/expenses_tracker/expenses_tracker/web/models.py
+++ Python Web Basics/Exam Preparation/expenses_tracker/expenses_tracker/web/models.py
@@ -1,3 +1,70 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from expenses_tracker.web.validators import validate_only_letters, MaxFileSizeInMbValidator
+
+
+class Profile(models.Model):
+    NAME_MAX_LEN = 15
+    NAME_MIN_LEN = 2
+
+    BUDGET_MIN_VALUE = 0
+
+    IMAGE_MAX_SIZE_IN_MB = 5
+    IMAGE_UPLOAD_TO_DIR = 'profiles/'
+
+    first_name = models.CharField(
+        max_length=NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(NAME_MIN_LEN),
+            validate_only_letters,
+        ),
+    )
+
+    last_name = models.CharField(
+        max_length=NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(NAME_MIN_LEN),
+            validate_only_letters,
+        ),
+    )
+
+    budget = models.FloatField(
+        default=BUDGET_MIN_VALUE,
+        validators=(
+            MinValueValidator(BUDGET_MIN_VALUE),
+        )
+    )
+
+    image = models.ImageField(
+        upload_to=IMAGE_UPLOAD_TO_DIR,
+        null=True,
+        blank=True,
+        validators=(
+            MaxFileSizeInMbValidator(IMAGE_MAX_SIZE_IN_MB),
+        ),
+    )
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Expense(models.Model):
+    TITLE_MAX_LENGTH = 30
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+    )
+
+    image = models.URLField()
+
+    price = models.FloatField()
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ('title', 'price')
