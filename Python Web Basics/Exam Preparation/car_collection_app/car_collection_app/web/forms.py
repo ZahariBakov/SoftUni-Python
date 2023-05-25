562d7dc2d1844100d11688d8d7b879b6c423273b
ZahariBakov <zaharibakov@gmail.com>
2023-05-25 18:26:25 +0300

    Update new Django project "car_collection_app".

--- Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/forms.py
+++ Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/forms.py
@@ -11,7 +11,34 @@ class ProfileForm(forms.ModelForm):
         fields = ['username', 'email', 'age', 'password']
 
 
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
 class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+
+class EditCarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+
+class DeleteCarForm(CarForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for (_, field) in self.fields.items():
+            # field.widget.attrs['disabled'] = 'disabled'
+            field.widget.attrs['readonly'] = 'readonly'
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+
+        return self.instance
--- Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/views.py
+++ Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from car_collection_app.web.forms import ProfileForm, CarForm
+from car_collection_app.web.forms import ProfileForm, CarForm, EditCarForm, DeleteCarForm, EditProfileForm
 from car_collection_app.web.models import Profile, Car
 
 
@@ -39,15 +39,52 @@ def profile_create(request):
 
 
 def profile_details(request):
-    return render(request, 'profile-details.html')
+    profile = get_profile()
+    cars = Car.objects.all()
+    total_cars_price = sum(c.price for c in cars)
+
+    context = {
+        'profile': profile,
+        'total_cars_price': total_cars_price,
+    }
+
+    return render(request, 'profile-details.html', context)
 
 
 def profile_edit(request):
-    return render(request, 'profile-edit.html')
+    profile = get_profile()
+
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=profile)
+
+        if form.is_valid():
+            form.save()
+            return redirect('profile details')
+
+    form = EditProfileForm(instance=profile)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'profile-edit.html', context)
 
 
 def profile_delete(request):
-    return render(request, 'profile-details.html')
+    profile = get_profile()
+    cars = Car.objects.all()
+
+    if request.method == 'POST':
+        profile.delete()
+        cars.delete()
+
+        return redirect('index')
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profile-delete.html', context)
 
 
 def catalogue(request):
@@ -82,12 +119,51 @@ def car_create(request):
 
 
 def car_details(request, pk):
-    return render(request, 'car-details.html')
+    car = Car.objects.get(pk=pk)
+
+    context = {
+        'car': car,
+        'profile': Profile.objects.get(),
+    }
+    return render(request, 'car-details.html', context)
 
 
 def car_edit(request, pk):
-    return render(request, 'car-edit.html')
+    car = Car.objects.filter(pk=pk).get()
+
+    if request.method == 'POST':
+        form = EditCarForm(request.POST, instance=car)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+
+    form = EditCarForm(instance=car)
+
+    context = {
+        'form': form,
+        'profile': Profile.objects.first(),
+        'car': car,
+    }
+
+    return render(request, 'car-edit.html', context)
 
 
 def car_delete(request, pk):
-    return render(request, 'car-delete.html')
+    car = Car.objects.filter(pk=pk).get()
+
+    if request.method == 'GET':
+        form = DeleteCarForm(instance=car)
+
+    else:
+        form = DeleteCarForm(request.POST, instance=car)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+
+    context = {
+        'form': form,
+        'profile': get_profile(),
+        'car': car,
+    }
+
+    return render(request, 'car-delete.html', context)
