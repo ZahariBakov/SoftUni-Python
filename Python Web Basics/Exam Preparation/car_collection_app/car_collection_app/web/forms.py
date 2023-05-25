3dab2825de114b7ddf29459c8d6442b4d753bdd0
ZahariBakov <zaharibakov@gmail.com>
2023-05-25 10:25:51 +0300

    Update new Django project "car_collection_app".

--- Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/forms.py
+++ Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 
+from car_collection_app.web.models import Profile, Car
+
 
 class ProfileForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Profile
+        fields = ['username', 'email', 'age', 'password']
+
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
--- Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/views.py
+++ Python Web Basics/Exam Preparation/car_collection_app/car_collection_app/web/views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from car_collection_app.web.forms import ProfileForm, CarForm
+from car_collection_app.web.models import Profile, Car
+
+
+def get_profile():
+    try:
+        return Profile.objects.get()
+    except:
+        return None
 
 
 def index(request):
-    return render(request, 'index.html')
+    profile = get_profile()
+
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'index.html', context)
 
 
 def profile_create(request):
-    return render(request, 'profile-create.html')
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('catalogue')
+
+    else:
+        form = ProfileForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'profile-create.html', context)
 
 
 def profile_details(request):
@@ -22,20 +51,43 @@ def profile_delete(request):
 
 
 def catalogue(request):
-    return render(request, 'catalogue.html')
+    cars = Car.objects.all()
+    profile = Profile.objects.get()
+    cars_count = cars.count()
+
+    context = {
+        'cars': cars,
+        'profile': profile,
+        'cars_count': cars_count
+    }
+
+    return render(request, 'catalogue.html', context)
 
 
 def car_create(request):
-    return render(request, 'car-create.html')
+    form = CarForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('catalogue')
+
+    profile = Profile.objects.get()
+
+    context = {
+        'form': form,
+        'profile': profile,
+    }
+
+    return render(request, 'car-create.html', context)
 
 
-def car_details(request):
+def car_details(request, pk):
     return render(request, 'car-details.html')
 
 
-def car_edit(request):
+def car_edit(request, pk):
     return render(request, 'car-edit.html')
 
 
-def car_delete(request):
+def car_delete(request, pk):
     return render(request, 'car-delete.html')
