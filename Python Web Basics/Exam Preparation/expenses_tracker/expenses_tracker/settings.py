f20e00c8314018769543d709525786b876232bc0
ZahariBakov <zaharibakov@gmail.com>
2023-06-03 09:29:07 +0300

    Update project "expenses_tracker".

--- Python Web Basics/Exam Preparation/expenses_tracker/expenses_tracker/settings.py
+++ Python Web Basics/Exam Preparation/expenses_tracker/expenses_tracker/settings.py
@@ -110,7 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'expenses_static/'
+
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
