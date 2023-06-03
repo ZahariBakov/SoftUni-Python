cd8f6e2dafca0a5da1f2e69c4422a32560f21118
ZahariBakov <zaharibakov@gmail.com>
2023-06-03 09:10:20 +0300

    Update project "my_music_app".

--- Python Web Basics/Exam Preparation/my_music_app/my_music_app/settings.py
+++ Python Web Basics/Exam Preparation/my_music_app/my_music_app/settings.py
@@ -110,7 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'music_app_static/'
+
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
