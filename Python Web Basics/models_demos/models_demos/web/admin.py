a5b5b6f2a0a5b897e2f0f0ecc4adab2eae33f0b7
ZahariBakov <zaharibakov@gmail.com>
2023-06-13 17:36:44 +0300

    Update project "models_demos".

--- Python Web Basics/models_demos/models_demos/web/admin.py
+++ Python Web Basics/models_demos/models_demos/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from models_demos.web.models import Employee, NullBlankDemo
+
+
+# The `Employee` model is enabled in django admin
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NullBlankDemo)
+class NullBlankDemoAdmin(admin.ModelAdmin):
+    pass
--- Python Web Basics/models_demos/models_demos/web/models.py
+++ Python Web Basics/models_demos/models_demos/web/models.py
@@ -1,5 +1,6 @@
-from django.db import models
+from enum import Enum
 
+from django.db import models
 
 # Model fields == class attributes in Model classes
 
@@ -13,7 +14,29 @@ SQL Server: money
 '''
 
 
+# class EmployeeLevel(Enum):
+#     JUNIOR = 'Junior',
+#     REGULAR = 'Regular',
+#     SENIOR = 'Senior'
+
+
+class Department(models.Model):
+    name = models.CharField(
+        max_length=15,
+    )
+
+
 class Employee(models.Model):
+    LEVEL_JUNIOR = 'Junior'
+    LEVEL_REGULAR = 'Regular'
+    LEVEL_SENIOR = 'Senior'
+
+    LEVELS = (
+        (LEVEL_JUNIOR, LEVEL_JUNIOR),
+        (LEVEL_REGULAR, LEVEL_REGULAR),
+        (LEVEL_SENIOR, LEVEL_SENIOR),
+    )
+
     # Var char (30) => strings with max length 30
     first_name = models.CharField(
         max_length=30,
@@ -25,10 +48,14 @@ class Employee(models.Model):
     )
 
     level = models.CharField(
-        max_length=15,
+        max_length=len(LEVEL_REGULAR),
+        choices=LEVELS,
+        verbose_name='Seniority level'
     )
 
-    age  = models.IntegerField()
+    age = models.IntegerField(
+        default=-7,
+    )
 
     # Int => 0
     years_of_experience = models.PositiveIntegerField()
@@ -38,7 +65,15 @@ class Employee(models.Model):
 
     start_date = models.DateField()
 
-    email = models.EmailField()
+    email = models.EmailField(
+        # Adds `UNIQUE` constraint
+        unique=True,
+        editable=False,
+    )
+
+    is_full_time = models.BooleanField(
+        null=True,
+    )
 
     # This will be automatically set on creation
     created_on = models.DateTimeField(
@@ -50,6 +85,18 @@ class Employee(models.Model):
         auto_now=True,
     )
 
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+    )
+
+    @property
+    def fullname(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return f'Id: {self.pk}; Name: {self.fullname}'
+
 
 # Employee.objects.raw('SELECT *')  # raw SQL
 # Employee.objects.all()            # Select
@@ -58,5 +105,26 @@ class Employee(models.Model):
 # Employee.objects.update()         # Update
 
 '''
-Django ORM (Object-relation mapping
+Django ORM (Object-relation mapping)
 '''
+
+
+class NullBlankDemo(models.Model):
+    # blank = models.IntegerField(
+    #     blank=True,
+    #     null=False,
+    # )
+    # null = models.IntegerField(
+    #     blank=False,
+    #     null=True,
+    # )
+    blank_null = models.IntegerField(
+        blank=True, # Form validation
+        null=True,
+    )
+    default = models.IntegerField()
+    # --- SAME ---
+    # default = models.IntegerField(
+    #     blank=False,
+    #     null=False,
+    # )
