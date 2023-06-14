077fc2e7477da95c8cdda8da04e9f2495f5f2bb1
ZahariBakov <zaharibakov@gmail.com>
2023-06-14 17:32:05 +0300

    Update project "models_demos".

--- Python Web Basics/models_demos/models_demos/web/urls.py
+++ Python Web Basics/models_demos/models_demos/web/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = ()
+from django.urls import path
+
+from models_demos.web.views import index
+
+urlpatterns = (
+    path('', index, name='index'),
+)
--- Python Web Basics/models_demos/models_demos/web/views.py
+++ Python Web Basics/models_demos/models_demos/web/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+from models_demos.web.models import Employee, Department
+
+
+def index(request):
+    x = list(Employee.objects.all())
+    print(list(User.objects.all()))
+    print(list(Department.objects.all()))
+    print(x)
+
