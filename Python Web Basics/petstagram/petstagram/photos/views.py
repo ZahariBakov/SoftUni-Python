247d9d3fe45b67bff3b46bc8b6f1e5cf5c7a2b48
ZahariBakov <zaharibakov@gmail.com>
2023-06-16 16:07:32 +0300

    Create Django project "petstagram". Add models to Home Page. Implement Like button Functionality. Implement Share button Functionality. Add models to "Pet details" page. Add models to "Photo details" page. Order comments.

--- Python Web Basics/petstagram/petstagram/photos/views.py
+++ Python Web Basics/petstagram/petstagram/photos/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
+from petstagram.photos.models import Photo
+
 
 def add_photo(request):
     return render(request, template_name='photos/photo-add-page.html')
 
 
-def photo_details(request):
-    return render(request, template_name='photos/photo-details-page.html')
+def photo_details(request, pk):
+    photo = Photo.objects.filter(pk=pk).get()
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments
+    }
+
+    return render(request, 'photos/photo-details-page.html', context)
 
 
 def edit_photo(request):
