727b40e9fbf2603f24053df774f97cc248acdb9b
ZahariBakov <zaharibakov@gmail.com>
2023-09-06 17:20:52 +0300

    Update exercise "hot_potato".

--- Pyhon Advanced/Lists as Stacks and Queues - Lab/hot_potato.py
+++ Pyhon Advanced/Lists as Stacks and Queues - Lab/hot_potato.py
@@ -1,21 +1,30 @@
 from collections import deque
 
-players_string = input()
-tosses_count = int(input())
+# players_string = input()
+# tosses_count = int(input())
+#
+# player = deque(players_string.split(" "))
+# current_toss = 0
+#
+# while len(player) > 1:
+#     current_toss += 1
+#     current_player = player.popleft()
+#
+#     if current_toss == tosses_count:
+#         current_toss = 0
+#         print(f"Removed {current_player}")
+#
+#     else:
+#         player.append(current_player)
+#
+# winner = player.popleft()
+# print(f"Last is {winner}")
 
-player = deque(players_string.split(" "))
-current_toss = 0
+kids = deque(input().split())
+n = int(input())
 
-while len(player) > 1:
-    current_toss += 1
-    current_player = player.popleft()
+while len(kids) > 1:
+    kids.rotate(-n)
+    print(f"Removed {kids.pop()}")
 
-    if current_toss == tosses_count:
-        current_toss = 0
-        print(f"Removed {current_player}")
-
-    else:
-        player.append(current_player)
-
-winner = player.popleft()
-print(f"Last is {winner}")
+print(f"Last is {kids.popleft()}")
